266d261e2325d8510924d6b9c3fe94b40010bc0e
SeungHwanYuk <ysh54244902@gmail.com>
2024-08-28 00:43:53 +0900

    0828

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -17,14 +17,26 @@ import {
 import { useNavigate } from "react-router-dom";
 
 function Main() {
+
   const navigate = useNavigate();
+
+  function loginCheck() {
+    let token = localStorage.getItem("JWT-token");
+    if(token)
+    {
+      navigate(`/playgame`)
+    } else {
+      alert("로그인이 필요합니다.")
+    }
+  }
+
   return (
     <>
       <Header />
       <Wrapper ju={`center`} al={`flex-end`} dr={`column`}>
         <Image src="/images/testBanner.png" />
         <Wrapper dr={`column`} al={`flex-end`} isAbsolute top={`32%`}>
-          <GameStartButton onClick={() => navigate(`/playgame`)}>
+          <GameStartButton onClick={() => loginCheck()}>
             GAME START
           </GameStartButton>
           <Profile />
